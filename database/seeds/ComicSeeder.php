be6340421117e86b87bd9437a24fcda55d591165
matteopelusi7 <pelusimatteo02@gmail.com>
2022-04-26 15:53:42 +0200

    Add migrate and seeder

---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Comic;
+use Illuminate\Database\Seeder;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics = config('comics');
+
+        foreach ( $comics as $c ) {
+
+            $comic = new Comic();
+
+            $comic->title = $c['title'];
+            $comic->description = $c['description'];
+            $comic->img = $c['thumb'];
+            $comic->price = $c['price'];
+            $comic->series = $c['series'];
+            $comic->date = $c['sale_date'];
+            $comic->type = $c['type'];
+
+            $comic->save();
+
+        }
+    }
+}
