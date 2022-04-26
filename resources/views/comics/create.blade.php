6793ccc734cb1047362fb3dee1cd248d04945d01
matteopelusi7 <pelusimatteo02@gmail.com>
2022-04-26 16:56:58 +0200

    Add create

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('mainContent')
+
+    <main>
+
+        <div>
+
+            <h1>Crea nuovo Comic</h1>
+
+            <form action="{{ route('comics.store') }}" method="POST">
+
+                <div>
+                    <label for="title">Titolo:</label>
+                    <input type="text" id="title" name="title" placeholder="Inserisci il titolo">
+                </div>
+                <div>
+                    <label for="description">Descrizione:</label>
+                    <textarea name="description" id="description" cols="30" rows="10"></textarea>
+                </div>
+                <div>
+                    <label for="img">Descrizione:</label>
+                    <input type="text" id="img" name="img" placeholder="Inserisci URL immagine">
+                </div>
+                <div>
+                    <label for="price">Prezzo:</label>
+                    <input type="text" id="price" name="price" placeholder="Inserisci il prezzo">
+                </div>
+                <div>
+                    <label for="series">Serie:</label>
+                    <input type="text" id="series" name="series" placeholder="Inserisci la serie">
+                </div>
+                <div>
+                    <label for="date">Data:</label>
+                    <input type="text" id="date" name="date" placeholder="Inserisci la data di uscita">
+                </div>
+                <div>
+                    <label for="type">Tipo:</label>
+                    <select name="type" id="type">
+                        <option value="comic_book">Comic Book</option>
+                        <option value="graphic_novel">Graphic Novel</option>
+                    </select>
+                </div>
+
+                <button type="submit">
+                    Crea Comic
+                </button>
+
+            </form>
+
+        </div>
+
+    </main>
+    
+@endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,6 +5,9 @@
             <li>
                 <a href="{{ route('comics.index') }}">Comics</a>
             </li>
+            <li>
+                <a href="{{ route('comics.create') }}">Crea nuovo Comic</a>
+            </li>
         </ul>
     </nav>
 
